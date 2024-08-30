6a57eb4c23cfbae265589a11c6e8feccaf8a4c7e
mhmd-aashik <aashikdevelop@gmail.com>
2024-08-30 18:39:36 +0530

    number change

--- app/page.tsx
+++ app/page.tsx
@@ -251,7 +251,7 @@ export default function Home() {
             >
               <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
             </svg>
-            <span>+94 772390100</span>
+            <span>074-297-8696</span>
           </a>
           <a
             href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRzBzFvgjjwPKQHbNtvlBjTJstgQCBXrsqwVbhKxbwNJPdWvxfGgnLjGHfkKlNpvJLnKvhJH"
@@ -268,7 +268,7 @@ export default function Home() {
               <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
               <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
             </svg>
-            <span>Aziyan@gmail.com</span>
+            <span>gallepaint@gmail.com</span>
           </a>
         </div>
       </div>
